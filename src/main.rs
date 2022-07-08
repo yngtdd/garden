a5be6b12b0af47e5f85ae3f4e32eb6c31d0df231
Todd Young <yngtdd@gmail.com>
2022-07-08 10:33:27 -0400

    feat: Add writing functionality

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-use color_eyre::eyre::Result;
+use std::path::PathBuf;
+
+use color_eyre::eyre::{eyre, Result, WrapErr};
+use directories::UserDirs;
 use structopt::StructOpt;
+
+use garden::write;
 
 /// A CLI for growing and curating a digital garden
 ///
@@ -7,6 +12,9 @@ use structopt::StructOpt;
 #[derive(StructOpt, Debug)]
 #[structopt(name = "garden")]
 struct Opt {
+    #[structopt(parse(from_os_str), short = "p", long, env)]
+    garden_path: Option<PathBuf>,
+
     #[structopt(subcommand)]
     cmd: Command,
 }
@@ -24,12 +32,26 @@ enum Command {
     },
 }
 
+fn get_default_garden_dir() -> Result<PathBuf> {
+    let user_dirs = UserDirs::new()
+        .ok_or_else(|| eyre!("Could not find home directory!"))?;
+
+    Ok(user_dirs.home_dir().join(".garden"))
+}
+
 fn main() -> Result<()> {
     color_eyre::install()?;
     let opt = Opt::from_args();
     dbg!(&opt);
 
+    let garden_path = match opt.garden_path {
+        Some(pathbuf) => Ok(pathbuf),
+        None => {
+            get_default_garden_dir().wrap_err("`garden_path` was not supplied")
+        }
+    }?;
+
     match opt.cmd {
-        Command::Write { title: _ } => todo!(),
+        Command::Write { title } => write(garden_path, title),
     }
 }
---
+++ src/write.rs
@@ -0,0 +1,6 @@
+use color_eyre::Result;
+use std::path::PathBuf;
+
+pub fn write(garden_path: PathBuf, title: Option<String>) -> Result<()> {
+    todo!()
+}
